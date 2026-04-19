c3f143c67635cce04b06461ed22ecd266e22304e
Andrew Baxter <>
2026-04-20 03:13:44 +0900

    Initial pt 2

--- source/src/lib.rs
+++ source/src/lib.rs
@@ -7,30 +7,47 @@ use {
 };
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub enum SchemaskElement {
+pub enum Maskoid {
+    /// Match null.
+    Null,
+    /// Match any string.
     String,
-    ConstString,
+    /// Must be exactly this string.
+    ConstString(String),
+    /// Match any bool.
     Bool,
-    ConstBool,
+    /// Match any int.
     Int,
-    ConstInt,
+    /// Match any float.
     Float,
-    ConstFloat,
-    /// Use the schemask element in the root `bindings` map with this name.
+    /// Match the maskoid in the root `bindings` map with this name.
     Ref(String),
-    Option(Box<SchemaskElement>),
-    /// Homogenous set, unordered, no duplicates
-    Set(Box<SchemaskElement>),
-    List(Box<SchemaskElement>),
-    StringMap(Box<SchemaskElement>),
-    Tuple(Vec<SchemaskElement>),
-    TaggedUnion(HashMap<String, SchemaskElement>),
-    Record(HashMap<String, SchemaskElement>),
+    /// The value may match the maskoid or it may be null. In a record, the associated
+    /// key may also be missing. If options are nested, the inner option is treated
+    /// like a single element record with the key "element".
+    Option(Box<Maskoid>),
+    /// Homogenous set where every element matches this maskoid. Unordered, no
+    /// duplicates.
+    Set(Box<Maskoid>),
+    /// Homogenous list where every element matches this maskoid. Preserves order.
+    List(Box<Maskoid>),
+    /// Homogenous map where keys are strings and every value matches this maskoid.
+    /// Preserves order.
+    StringMap(Box<Maskoid>),
+    /// Heterogenous list where the entries are matched pairwise with a same-length
+    /// list of maskoids.
+    Tuple(Vec<Maskoid>),
+    /// Matches exactly one maskoid, based on a tag (the key). In json, this is encoded
+    /// as a single key object `{KEY: ELEMENT}`.
+    TaggedUnion(HashMap<String, Maskoid>),
+    /// Each value is matched against the maskoid with the corresponding key. There's
+    /// no match if there are extra elements or missing elements.
+    Record(HashMap<String, Maskoid>),
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Schemask {
-    pub bindings: HashMap<String, SchemaskElement>,
+    pub bindings: HashMap<String, Maskoid>,
     /// Which bound element to validate against if none are explicitly specified
     pub default: Option<String>,
 }
